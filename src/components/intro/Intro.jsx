be97df475573b12c371ccb8627f78f4462c7e038
Ahmed Faraz <ahmedjamshed79@gmail.com>
2021-07-10 22:03:38 +1000

    added some assets. Made some changes to the intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,19 +1,33 @@
 import './Intro.scss';
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed:60,
+        strings: ["Freelancer", "Web App Developer", "Front-End with React.js"],
+      });
+    }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="Assets/man.png" alt=""/>
+                    {/* <img src="Assets/man.png" alt=""/> */}
                 </div>
              </div>
             <div className="right">
                  <div className="wrapper">
                     <h2>Hi There, I'm</h2>
-                    <h1>Safak Kocaoglu</h1>
+                    <h1>Ahmed Faraz</h1>
                      <h3>
-                        Freelance <span> Develoepr </span>
+                        <span ref={textRef}> </span>
                     </h3>
                 </div>
                 <a href="#portfolio">
