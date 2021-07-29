2d41d0cfb80009c0de64797f2bea8f488af8f7b7
Ahmed Faraz <ahmedjamshed79@gmail.com>
2021-07-30 00:07:23 +1000

    New color theme added in golbal.scss. Navbar scheme changed according to new theme.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,25 +1,31 @@
-import './NavBar.scss';
+import "./NavBar.scss";
 
-function NavBar({menuOpen, setMenuOpen}) {
-    return (
-        <div className={"navbar " + (menuOpen && "active")}>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo">Ahmed.</a>
-                </div>
+function NavBar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"navbar " + (menuOpen && "active")}>
+      <div className="left">
+        <a href="#intro" className="logo">
+          Ahmed.
+        </a>
+      </div>
 
-                <div className="right">
-                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                </div>
+      <div className="center">
+          <a href="#intro">Home</a>
+          <a href="#about">About</a>
+          <a href="#skills">Skills</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#contact">Contact me</a>
+      </div>
 
-            </div>
-            
+      <div className="right">
+        <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          <span className="line1"></span>
+          <span className="line2"></span>
+          <span className="line3"></span>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default NavBar; 
+export default NavBar;
