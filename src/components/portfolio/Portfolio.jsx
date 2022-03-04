2453e582b9c578f34690f07db2c38158f3c65e8c
Ahmed Faraz <ahmedjamshed79@gmail.com>
2022-03-04 14:15:02 +0500

    changed aos animation that was causing issue with html width

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -53,7 +53,7 @@ function Portfolio() {
       <h2>My Works</h2>
       <div className="container">
         {featuredPortfolio.map((item) => (
-          <div className="item" key={item.id} data-aos={item.id % 2 === 0 ? "fade-right" : "fade-left" } data-aos-anchor="#portfolio">
+          <div className="item" key={item.id} data-aos={item.id % 2 === 0 ? "flip-right" : "flip-left" }>
             <figure onClick={() => onClickItem(item.id)}>
               <img src={item.img} alt="" />
               <div className="overlay">
